f40b29311a3dfea1815c8c0f732e005befeb9161
simple <shwncho4@gmail.com>
2022-03-12 00:04:28 +0900

    Feat: 값 타입 매핑

--- src/main/java/jpabook/jpashop/domian/Member.java
+++ src/main/java/jpabook/jpashop/domian/Member.java
@@ -13,17 +13,14 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
 
-    private String createBy;
-    private LocalDateTime createDate;
-    private String lastModifiedBy;
-    private LocalDateTime lastModifiedDate;
+    @Embedded
+    private Address address;
+
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
 
     public Long getId() {
         return id;
@@ -41,27 +38,19 @@ public class Member {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String getStreet() {
-        return street;
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
